5a3d3d1147ca7b9be60f6ee363cf8becc4e17637
MjrKong42 <mjr.kong42@gmail.com>
2021-04-15 10:07:16 -0500

    ReactAppExercise with bonus

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // 2b. Open the App.js file and add the necessary import line at the top of the file that allows the Exercise component to be used in the App.js file
 import Exercise from './exercise/Exercise'
+// 6g. Open the App.js file and create the Bonus import below the Exercise import at the top of the file
+import Bonus from './bonus/Bonus'
 
 function App() {
   return (
@@ -11,6 +13,10 @@ function App() {
 // 1st Prop: Set the name of the prop to "good". Set the value to the following string value "Awesome"
 // 2nd Prop: Set the name of the prop to "bad". Set the value to the following string value "Terrible"*/}
     <Exercise good='Awesome' bad='Terrible' />
+
+
+{/*// 6h. Inside the return for the App component, pass in the Bonus component below the Exercise component so it will render to the browser*/}
+<Bonus />
     
     </div>
   );
